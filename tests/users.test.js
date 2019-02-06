735153dd6a12703b6fbb1300fe7b4e0393c47714
Kevin Peña <kevin.pena.prog@gmail.com>
2019-02-06 00:11:32 -0500

    Modify mongoose models, schemas and types to conform to spec

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const app = require('../app')
-const { deleteAll, addTestUsers, addTestSell, checkResponse, checkUser, checkSell } = require('./utils')
+const { deleteAll, checkResponse } = require('./utils')
 chai.should()
 chai.use(chaiHttp)
 
@@ -11,55 +11,13 @@ describe('User routes', () => {
     await deleteAll()
   })
 
-  it('GET / should return an array', async () => {
+  it('GET /users should return an empty array if there is no users', async () => {
     const res = await chai.request(app).get('/users')
-    
+
     checkResponse(res)
     res.body.users.should.be.an('array')
     res.body.users.length.should.eql(0)
+    
   })
 
-  it('GET / should return users if they are in the db', async () => {
-    await addTestUsers()
-
-    const res = await chai.request(app).get('/users')
-
-    checkResponse(res)
-    res.body.users.should.be.an('array')
-    const users = res.body.users
-    users.should.have.lengthOf(2)
-    users.forEach(checkUser)
-  })
-
-  it('GET /:id should return individual user', async () => {
-    const inserted = await addTestUsers()
-    const id = inserted[0]._id
-
-    const res = await chai.request(app).get(`/users/${id}`)
-
-    checkResponse(res)
-    res.body.user.should.be.an('object')
-
-    const user = res.body.user
-    checkUser(user)
-  })
-
-  it('GET /:id with query param should yield user with sells', async () => {
-    const inserted = await addTestUsers()
-    const id = inserted[0]._id
-    await addTestSell(id)
-
-    const res = await chai.request(app).get(`/users/${id}?includeSells=true`)
-
-    checkResponse(res)
-    res.body.user.should.be.an('object')
-
-    const user = res.body.user
-    user.should.be.an('object')
-    user.sells.should.be.an('array')
-    user.sells.should.have.lengthOf(1)
-    const sell = user.sells[0]
-    checkSell(sell)
-  })
 })
-
