53dfe4eb214349e7a1c5a3c051aab06ed2410f0d
Kevin Peña <kevin.pena.prog@gmail.com>
2019-02-05 23:50:37 -0500

    Config production database provided in the challenge description

--- db/config.js
+++ db/config.js
@@ -3,12 +3,17 @@ module.exports = (env = process.env.NODE_ENV) => {
   return {
     development: {
       db: {
-        connection: 'mongodb://localhost:27017/db-dev',
+        connection: 'mongodb://localhost:27017/db-moadw-dev',
       },
     },
     test: {
       db: {
-        connection: 'mongodb://localhost:27017/db-test',
+        connection: 'mongodb://localhost:27017/db-moadw-test',
+      },
+    },
+    production: {
+      db: {
+        connection: 'mongodb+srv://root:O7DgnKqSqCFmvC6n@tests-0eeni.mongodb.net/test',
       },
     },
   }[env || 'development']
